9f1c5d51dcd4670cd0a926985cc7ffaaf34661e2
CPUPetet <c.nwodo@abujaprep.com>
2026-05-10 17:19:59 +0100

    break

---
+++ break.py
@@ -0,0 +1,7 @@
+A = str(input("Enter a word or letter in full caps: "))
+for i in A:
+    if (i == 'A'):
+        print("A was found here.")
+        break
+    else:
+        print("A was not found here.")
